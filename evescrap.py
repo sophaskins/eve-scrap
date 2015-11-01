13f7d595c38dec8db8ead7e6665f75c6cb543700
Sophie Haskins <haskins.sophie@gmail.com>
2015-11-01 14:54:02 -0500

    sort of pep8

--- evescrap.py
+++ evescrap.py
@@ -3,6 +3,7 @@ import operator
 import requests
 import cachetools
 from xml.etree import ElementTree
+from functools import reduce
 
 
 price_cache = cachetools.TTLCache(10000, 86400)
@@ -33,16 +34,16 @@ def typeid_to_minerals(type_id):
 
     cursor = eve_sde.cursor()
     cursor.execute("""SELECT
-        SUM(CASE WHEN m1.materialTypeID = 34 THEN m1.quantity ELSE 0 END) AS Tritanium, 
-        SUM(CASE WHEN m1.materialTypeID = 35 THEN m1.quantity ELSE 0 END) AS Pyerite, 
-        SUM(CASE WHEN m1.materialTypeID = 36 THEN m1.quantity ELSE 0 END) AS Mexallon, 
-        SUM(CASE WHEN m1.materialTypeID = 37 THEN m1.quantity ELSE 0 END) AS Isogen, 
-        SUM(CASE WHEN m1.materialTypeID = 38 THEN m1.quantity ELSE 0 END) AS Nocxium, 
-        SUM(CASE WHEN m1.materialTypeID = 39 THEN m1.quantity ELSE 0 END) AS Zydrine, 
-        SUM(CASE WHEN m1.materialTypeID = 40 THEN m1.quantity ELSE 0 END) AS Megacyte, 
-        SUM(CASE WHEN m1.materialTypeID = 11399 THEN m1.quantity ELSE 0 END) AS Morphite 
-    FROM invTypes t1 
-    INNER JOIN invTypeMaterials m1 ON t1.typeID = m1.typeID 
+        SUM(CASE WHEN m1.materialTypeID = 34 THEN m1.quantity ELSE 0 END) AS Tritanium,
+        SUM(CASE WHEN m1.materialTypeID = 35 THEN m1.quantity ELSE 0 END) AS Pyerite,
+        SUM(CASE WHEN m1.materialTypeID = 36 THEN m1.quantity ELSE 0 END) AS Mexallon,
+        SUM(CASE WHEN m1.materialTypeID = 37 THEN m1.quantity ELSE 0 END) AS Isogen,
+        SUM(CASE WHEN m1.materialTypeID = 38 THEN m1.quantity ELSE 0 END) AS Nocxium,
+        SUM(CASE WHEN m1.materialTypeID = 39 THEN m1.quantity ELSE 0 END) AS Zydrine,
+        SUM(CASE WHEN m1.materialTypeID = 40 THEN m1.quantity ELSE 0 END) AS Megacyte,
+        SUM(CASE WHEN m1.materialTypeID = 11399 THEN m1.quantity ELSE 0 END) AS Morphite
+    FROM invTypes t1
+    INNER JOIN invTypeMaterials m1 ON t1.typeID = m1.typeID
     WHERE m1.typeID = ?""", (type_id,))
 
     result = cursor.fetchone()
@@ -50,6 +51,7 @@ def typeid_to_minerals(type_id):
     if result[0] == None:
         result = (0, 0, 0, 0, 0, 0, 0, 0)
     return result
+
 
 def calculate_refined_value(type_id, refining_multiplier):
     materials = typeid_to_minerals(type_id)
@@ -67,14 +69,14 @@ def calculate_reprocessing_rate(station_base_rate, tax_rate, scrapmetal_skill_le
 
 def lookup_mineral_prices():
     return (
-        lookup_market_price(34), # Tritanium
-        lookup_market_price(35), # Pyerite
-        lookup_market_price(36), # Mexallon
-        lookup_market_price(37), # Isogen
-        lookup_market_price(38), # Nocxium
-        lookup_market_price(39), # Zydrine
-        lookup_market_price(40), # Megacyte
-        lookup_market_price(11399), # Morphite
+        lookup_market_price(34),  # Tritanium
+        lookup_market_price(35),  # Pyerite
+        lookup_market_price(36),  # Mexallon
+        lookup_market_price(37),  # Isogen
+        lookup_market_price(38),  # Nocxium
+        lookup_market_price(39),  # Zydrine
+        lookup_market_price(40),  # Megacyte
+        lookup_market_price(11399),  # Morphite
     )
 
 
@@ -88,6 +90,7 @@ def lookup_market_price(type_id):
     price = xml_tree.find('.//buy/percentile').text
     return float(price)
 
+
 def appraise(items):
     refining_multiplier = 1
     parsed_out = []
@@ -100,27 +103,26 @@ def appraise(items):
             pass
     return parsed_out
 
+
 def appraise_item(item):
     refining_multiplier = 1
     appraisal = None
     typeID = typename_to_typeid(item['name'])
     if typeID:
-       market_price = lookup_market_price(typeID)
-       refined_value = calculate_refined_value(typeID, refining_multiplier)
+        market_price = lookup_market_price(typeID)
+        refined_value = calculate_refined_value(typeID, refining_multiplier)
 
-       if market_price > 0 and refined_value > 0:
-           refining_loss = (market_price - refined_value) / market_price
-           should_refine = refining_loss < 0.2
+        if market_price > 0 and refined_value > 0:
+            refining_loss = (market_price - refined_value) / market_price
+            should_refine = refining_loss < 0.2
 
-           appraisal = {
+            appraisal = {
                 'name': item['name'],
                 'quantity': item['quantity'],
                 'market_price': market_price,
                 'refined_value': refined_value,
                 'refining_loss': refining_loss * 100.0,
                 'refine': should_refine,
-           }
+            }
 
     return appraisal
-
-
